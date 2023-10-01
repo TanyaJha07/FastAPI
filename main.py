b9c3d262fe012ed28e5dad22f94106264d65b947
TanyaJha07 <tanyajhaanni@gmail.com>
2023-10-01 20:44:50 +0530

    crud on post

--- main.py
+++ main.py
@@ -1,19 +1,81 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI,Response, status, HTTPException, Depends
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+from random import randrange
+
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
+            {"title": "favorite foods", "content": "pizza, burger, fries", "id": 2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
+        
 @app.get('/') # "@' = decorator. it is used to add a new route to the API.(get = http method & '/' = path(route))
 async def root(): #async is used to make the function asynchronous(non-blocking)
     return {'message': 'WELCOME TO THE FIRST API'}
 
+
 @app.get('/posts')
 def get_posts():
-    return {"posts": "This is the posts"}
+    return {"posts": my_posts}
 
-@app.post('/createposts')
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"message": "Succesfully created posts"}
-   
+@app.post('/posts')
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 10000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+    # title str, content str
+
+
+@app.get('/posts/{id}')
+def get_post(id: int, response: Response):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                             detail=f"post with id: {id} was not found")
+    return {"post_details": post}
+
+@app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    # deleting the post
+    # find the index in the array that has required id
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                             detail=f"post with id: {id} does not exist")
+
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def uodate_post(id: int, post: Post):
+    print(post)
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                             detail=f"post with id: {id} does not exist")
+
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+
+    return {"data": post_dict}
