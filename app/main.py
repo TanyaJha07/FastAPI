762b77a58c7909cd4af038f3349580e5fa36657c
TanyaJha07 <tanyajhaanni@gmail.com>
2023-10-03 16:50:40 +0530

    database with models

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from . import models
-from .database import engine, SessionLocal
+from .database import engine,get_db
 from sqlalchemy.orm import Session
 
 models.Base.metadata.create_all(bind=engine)
@@ -15,12 +15,6 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 class Post(BaseModel):
     title: str
     content: str
